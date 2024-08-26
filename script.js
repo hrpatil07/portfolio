084e9e750a609927ce52da73967db5ad93db0a82
Hrushikesh R Patil <77801567+hrpatil07@users.noreply.github.com>
2024-08-26 20:18:09 +0000

    reverted the code

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 function toggleMenu() {
-  const menu = document.querySelector('.hamburger-menu');
-  menu.classList.toggle('active');
-}
-
-function toggleTheme() {
-  const body = document.body;
-  body.classList.toggle('dark-theme');
-  const themeToggle = document.querySelector('.theme-toggle');
-  if (body.classList.contains('dark-theme')) {
-    themeToggle.textContent = '🌕';
-  } else {
-    themeToggle.textContent = '🌙';
-  }
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
